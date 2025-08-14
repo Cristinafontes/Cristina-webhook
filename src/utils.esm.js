789649ff2ac5f653d645ca9ee1ce2461d838daff
Cristinafontes <secretariacristina25@gmail.com>
2025-08-13 21:18:52 -0300

    Delete src/utils.esm.js

--- src/utils.esm.js
+++
@@ -1,5 +0,0 @@
-export function parseDateTimeFromStrings(dateStr, timeStr) {
-  const [day, month, year] = dateStr.split("/").map(Number);
-  const [hour, minute] = timeStr.split(":").map(Number);
-  return new Date(2000 + year, month - 1, day, hour, minute);
-}
